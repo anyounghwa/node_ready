ba9456c966680061e34a3dc373c5975087670660
anyounghwa <e89cokr@naver.com>
2023-02-18 18:51:08 +0900

    32 App - post방식으로 전송된 데이터 받기

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var qs = require('querystring');
 
 function templateHTML(title,list, body) {
   return  `
@@ -198,6 +199,20 @@ var app = http.createServer(function(request,response){
         response.writeHead(200);
         response.end(template);
       });   
+   }
+   else if(pathname == '/create_process') {
+      var body = '';
+      request.on('data', function(data){
+        body = body + data;
+      });
+      request.on('end', function(data){
+        var post = qs.parse(body);
+        var title = post.title;
+        var description = post.description;
+        console.log(post);
+      });
+      response.writeHead(200);
+      response.end('success');
    }
    else {
     response.writeHead(404);
